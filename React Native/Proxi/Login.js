b2ac7ad04a3f13acddf82b9c85d58c4461b10714
Michael Salzman <mjsalz17@gmail.com>
2023-05-09 17:52:46 -0400

    Login UI update
    Got rid of group of photos... made one individual photo instead.
    
    Reformatted CSS

--- React Native/Proxi/Login.js
+++ React Native/Proxi/Login.js
@@ -7,13 +7,7 @@ export const Login = ({ navigation }) => {
   return (
     <SignUpContainerRoot>
       <SignUpBox>
-        <Group>
-          <Icon source={require("./assets/p.png")} />
-          <Icon1 source={require("./assets/r.png")} />
-          <Icon1 source={require("./assets/o.png")} />
-          <Icon3 source={require("./assets/x.png")} />
-          <Icon4 source={require("./assets/i.png")} />
-        </Group>
+        <Icon source={require("./assets/proxilogo.png")} />
         <SignUpOrLoginText>
           Sign up or login with your phone #
         </SignUpOrLoginText>
@@ -22,7 +16,7 @@ export const Login = ({ navigation }) => {
             <CountryCode>+1</CountryCode>
           </PhoneInputWrapper>
           <PhoneNumber
-            placeholder="7812010366"
+            placeholder="(781)201-0366"
             keyboardType="numeric"
             onChangeText={onChangePhoneNumber}
           />
@@ -38,12 +32,13 @@ export const Login = ({ navigation }) => {
 export const VerifyLogin = () => {};
 
 const SignUpContainerRoot = styled.View`
-  position: relative;
+  position: absolute;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
-  padding: 69px 0px 0px 0px;
-  border-radius: 20px;
+  padding: 60px 0px 0px 0px;
   box-sizing: border-box;
   background-color: #786cff;
   overflow: hidden;
@@ -51,69 +46,38 @@ const SignUpContainerRoot = styled.View`
 
 const SignUpBox = styled.View`
   width: 100%;
-  position: relative;
-  gap: 9px;
+  height: 100%;
+  position: static;
+  gap: 10px;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: start;
   align-items: center;
-  padding: 167px 30px 59px 30px;
-  border-radius: 20px;
+  padding: 60px 30px 60px 30px;
+  border-radius: 20px 20px 0px 0px;
   box-sizing: border-box;
   background-color: #ffffff;
 `;
 
-const Group = styled.View`
-  width: 173px;
-  position: relative;
-  gap: 4px;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: flex-start;
-  margin: 0px 0px 110px 0px;
-`;
+
 
 const Icon = styled.Image`
-  width: 36px;
-  min-width: 0px;
-  min-height: 0px;
-  position: relative;
-  flex-shrink: 0;
-  align-self: center;
+  width: 175px;
+  height: 73px;
+  object-fit: fill;
 `;
-const Icon1 = styled.Image`
-  width: 36px;
-  min-width: 0px;
-  min-height: 0px;
-  position: relative;
-  flex-shrink: 0;
-`;
-const Icon3 = styled.Image`
-  width: 36px;
-  min-width: 0px;
-  min-height: 0px;
-  position: relative;
-  flex-shrink: 0;
-  margin: 0px 4px 0px 0px;
-`;
-const Icon4 = styled.Image`
-  width: 9px;
-  min-width: 0px;
-  min-height: 0px;
-  position: relative;
-  flex-shrink: 0;
-`;
+
 const SignUpOrLoginText = styled.Text`
   position: relative;
   margin: 0px 0px 11px 0px;
   font-size: 13px;
   line-height: 17px;
   text-align: center;
+  margin: 60px 0px 10px 0px
 `;
 
 const PhoneNumberInput = styled.View`
-  width: 330px;
+  width: 300px;
   position: relative;
   gap: 10px;
   display: flex;
@@ -138,10 +102,10 @@ const PhoneInputWrapper = styled.View`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: -1px 0px;
+  margin: 0px 0px;
   padding: 14px 15px;
   border-width: 1px;
-  border-radius: 10px;
+  border-radius: 8px;
   border-style: solid;
   border-color: #000000;
 `;
