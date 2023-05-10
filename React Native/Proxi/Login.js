d7b1b610b82e71a7af18a4d632f7bbe7329b054f
Michael Salzman <mjsalz17@gmail.com>
2023-05-10 00:29:37 -0400

    Text-input update + CSS
    Updated text-input to reformat as you type.
    
    Made it so confirm button only turns red when there is a valid phone number.
    
    Large CSS update

--- React Native/Proxi/Login.js
+++ React Native/Proxi/Login.js
@@ -3,8 +3,28 @@ import { useState, useRef } from "react";
 import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 export const Login = ({ navigation }) => {
-  const [phoneNumber, onChangePhoneNumber] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [isValidPhoneNumber, setIsValidPhoneNumber] = useState(false);
   const inputRef = useRef();
+
+  const validatePhoneNumber = (phoneNumber) => {
+    let phoneRegex = /^[2-9]{2}[0-9]{8}$/; // sample phone number validation
+    return phoneRegex.test(phoneNumber);
+  };
+
+  const handlePhoneNumberChange = (text) => {
+    let cleaned = ('' + text).replace(/\D/g, '');
+    if(cleaned === "") {
+        setPhoneNumber("");
+        setIsValidPhoneNumber(false);
+        return;
+    }
+    let match = cleaned.match(/^(\d{0,3})(\d{0,3})(\d{0,4}).*/);
+    let number = ['(', match[1] || '', match[2] ? ') ' : '', match[2], match[3] ? '-' : '', match[3]].join('');
+  
+    setPhoneNumber(number);
+    setIsValidPhoneNumber(validatePhoneNumber(cleaned));
+};
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -23,12 +43,13 @@ export const Login = ({ navigation }) => {
                 ref={inputRef}
                 placeholder="(781)201-0366"
                 keyboardType="numeric"
-                onChangeText={onChangePhoneNumber}
+                value={phoneNumber}
+                onChangeText={handlePhoneNumberChange}
               />
             </PhoneNumberInput>
           </TouchableWithoutFeedback>
-          <ConfirmButton>
-            <ConfirmButtonText>Confirm</ConfirmButtonText>
+          <ConfirmButton active={isValidPhoneNumber}>
+            <ConfirmButtonText active={isValidPhoneNumber}>Confirm</ConfirmButtonText>
           </ConfirmButton>
         </SignUpBox>
       </SignUpContainerRoot>
@@ -37,46 +58,27 @@ export const Login = ({ navigation }) => {
 };
 
 export const VerifyLogin = () => {};
-
 const SignUpContainerRoot = styled.View`
-  position: absolute;
-  width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
   justify-content: flex-end;
-  padding: 20% 0px 0px 0px;
-  box-sizing: border-box;
   background-color: #786cff;
-  overflow: hidden;
 `;
 
 const SignUpBox = styled.View`
-  width: 100%;
-  height: 100%;
-  position: static;
-  gap: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
+  height: 85%;
+  justify-content: flex-start;
   align-items: center;
-  padding: 20% 30px 60px 30px;
-  border-radius: 20px 20px 0px 0px;
-  box-sizing: border-box;
+  padding: 60px 30px 60px 30px;
   background-color: #ffffff;
+  border-radius: 20px 20px 0px 0px;
 `;
-
-
 
 const Icon = styled.Image`
   width: 175px;
   height: 73px;
-  object-fit: fill;
 `;
 
 const SignUpOrLoginText = styled.Text`
-  position: relative;
-  margin: 0px 0px 11px 0px;
   font-size: 13px;
   line-height: 17px;
   text-align: center;
@@ -85,67 +87,48 @@ const SignUpOrLoginText = styled.Text`
 
 const PhoneNumberInput = styled.View`
   width: 300px;
-  position: relative;
-  gap: 10px;
-  display: flex;
   flex-direction: row;
   justify-content: flex-start;
-  align-self: stretch;
   align-items: center;
-  align-self: center;
+  gap: 10px;
   margin: 0px 0px 11px 0px;
   border-width: 1px;
   border-radius: 10px;
-  border-style: solid;
   border-color: #000000;
   background-color: #ffffff;
-  overflow: hidden;
 `;
+
 const PhoneInputWrapper = styled.View`
   width: 50px;
-  position: relative;
-  display: flex;
   flex-shrink: 0;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 0px 0px;
   padding: 14px 15px;
   border-width: 1px;
   border-radius: 8px;
-  border-style: solid;
   border-color: #000000;
 `;
+
 const CountryCode = styled.Text`
-  position: relative;
   line-height: 20px;
 `;
+
 const PhoneNumber = styled.TextInput`
-  position: relative;
   color: #828282;
   line-height: 20px;
 `;
+
 const ConfirmButton = styled.TouchableOpacity`
   width: 190px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 0px 0px 75px 0px;
-  padding: 0px;
-  padding-top: 16px;
-  padding-right: 61px;
-  padding-bottom: 16px;
-  padding-left: 61px;
-  border-width: 0px;
+  padding: 16px 61px;
   border-radius: 8px;
-  box-sizing: content-box;
-  background-color: #ff5a5f;
-  overflow: hidden;
-  cursor: pointer;
+  background-color: ${({ active }) => active ? '#ff5a5f' : '#e6e6e6'}; 
 `;
+
+
 const ConfirmButtonText = styled.Text`
-  color: #ffffff;
+  color: ${({ active }) => active ? '#ffffff' : 'grey'}; 
   font-weight: 600;
 `;
